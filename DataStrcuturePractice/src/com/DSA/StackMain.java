3b2a651f6f1e7ce0da3ea7b580277c2b680ce296
anand-krishanu <anandkrishanu26@gmail.com>
2024-09-28 13:34:53 +0530

    Stack Main Menu created

---
+++ DataStrcuturePractice/src/com/DSA/StackMain.java
@@ -0,0 +1,41 @@
+package com.DSA;
+
+import java.util.Scanner;
+
+public class StackMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Stack myStack = new Stack();
+
+        int choice;
+        
+        while (true) {
+            System.out.println("Choose an option: ");
+            System.out.println("1: Push      2: Pop      3: Print   4: Exit");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter a number to Push: ");
+                    int num = sc.nextInt();
+                    myStack.push(num);
+                    break;
+
+                case 2:
+                    if(myStack.isEmpty()) {
+                        System.out.println("Stack is Empty!");
+                    }
+                    myStack.pop();
+                    break;
+
+                case 3:
+                    myStack.printStack();
+                    break;
+
+                case 4:
+                    System.exit(0);
+            }
+        }
+    }
+}
