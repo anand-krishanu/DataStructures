8e6f8f266f3b297366fb92657ab07731f53b7141
anand-krishanu <anandkrishanu26@gmail.com>
2024-09-28 13:29:00 +0530

    Stack Implementation

--- DataStrcuturePractice/src/com/DSA/Stack.java
+++ DataStrcuturePractice/src/com/DSA/Stack.java
@@ -1,5 +1,54 @@
 package com.DSA;
 
 public class Stack {
-    
+    private Node top;
+    int height;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void printStack() {
+        Node temp = top;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public boolean isEmpty() {
+        return height == 0;
+    }
+
+    public void push(int value) {
+        Node newNode = new Node(value);
+
+        if (height == 0) {
+            top = newNode;
+            height = 1;
+        } else {
+            newNode.next = top;
+            top = newNode;
+            height++;
+        }
+    }
+
+    public Node pop() {
+        Node temp = top;
+
+        if (height == 0) {
+            return null;
+        }
+
+        top = top.next;
+        temp.next = null;
+
+        height--;
+        return temp;
+    }
 }
