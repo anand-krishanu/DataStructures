20ab2f553ad5221c1c724e36fa067899f01bff91
anand-krishanu <anandkrishanu26@gmail.com>
2024-10-01 12:53:31 +0530

    DLL Menu created

---
+++ DataStrcuturePractice/src/com/DLL/DLLMain.java
@@ -0,0 +1,57 @@
+package com.DLL;
+
+import com.LinkedList.LinkedList;
+
+import java.util.Scanner;
+
+public class DLLMain {
+    public static void main(String[] args) {
+        DoublyLinkedList myLL = new DoublyLinkedList();
+        Scanner sc = new Scanner(System.in);
+
+        int value;
+        int index;
+
+        while (true) {
+            System.out.println("Choose an option: ");
+            System.out.println("1: Insert Node at the end   2: Insert Node at the start   3: Insert   4: Delete First Node   5: Delete Last Node   6: Delete   7: Print LinkedList  8: End Code");
+            int choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter value to Insert at the end: ");
+                    value = sc.nextInt();
+                    myLL.append(value);
+                    break;
+                case 2:
+                    System.out.println("Enter value to Insert at the start: ");
+                    value = sc.nextInt();
+                    myLL.prepend(value);
+                    break;
+                case 3:
+                    System.out.println("Enter the index: ");
+                    index = sc.nextInt();
+                    System.out.println("Enter the value to insert: ");
+                    value = sc.nextInt();
+                    myLL.insert(index, value);
+                    break;
+                case 4:
+                    myLL.removeFirst();
+                    break;
+                case 5:
+                    myLL.removeLast();
+                    break;
+                case 6:
+                    System.out.println("Enter index to delete Node: ");
+                    index = sc.nextInt();
+                    myLL.delete(index);
+                    break;
+                case 7:
+                    myLL.printDLL();
+                    break;
+                case 8:
+                    System.exit(0);
+            }
+        }
+    }
+}
